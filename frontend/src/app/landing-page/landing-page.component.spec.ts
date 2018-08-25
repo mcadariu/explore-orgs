e28f1349f8b9cc2d9e3021e60efce26a47904e86
Mircea Cadariu <m.cadariu@sig.eu>
2018-08-25 22:39:05 +0200

    add unit test

--- frontend/src/app/landing-page/landing-page.component.spec.ts
+++ frontend/src/app/landing-page/landing-page.component.spec.ts
@@ -1,9 +1,34 @@
-describe('LandingPageComponent', () => {
+import {LandingPageComponent} from './landing-page.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+describe('LandingPageComponent', () => {
+  let component: LandingPageComponent;
+  let fixture: ComponentFixture<LandingPageComponent>;
+  let routerMock: Router;
 
   beforeEach(() => {
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
   });
 
-  it('should create an instance', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule, BrowserAnimationsModule],
+      declarations: [LandingPageComponent],
+      providers: [
+        {provide: Router, useValue: routerMock}
+      ]
+    });
+
+    fixture = TestBed.createComponent(LandingPageComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should navigate to the next route when clicking enter or on the button', () => {
+    fixture.detectChanges();
+    component.startExploreOrgs();
+    expect(routerMock.navigate).toHaveBeenCalled();
   });
 });
--- frontend/src/app/results/results.component.ts
+++ frontend/src/app/results/results.component.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
+  animations: [
+    trigger('fade-in', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+    ])
+  ]
 })
 export class ResultsComponent {
 
