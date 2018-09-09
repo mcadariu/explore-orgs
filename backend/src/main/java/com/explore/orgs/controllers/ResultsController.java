32714c4d814aa4817efd79127b8658e54bcbe6e6
Mircea Cadariu <m.cadariu@sig.eu>
2018-09-09 19:17:16 +0200

    revert

--- backend/src/main/java/com/explore/orgs/controllers/ResultsController.java
+++ backend/src/main/java/com/explore/orgs/controllers/ResultsController.java
@@ -5,9 +5,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ResultsController {
     @CrossOrigin
     @GetMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
--- backend/src/main/java/com/explore/orgs/controllers/ViewController.java
+++
@@ -1,9 +0,0 @@
-package com.explore.orgs.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-public class ViewController {
-    @RequestMapping({"/"})
-    public String index() {
-        return "forward:/index.html";
-    }
-}
